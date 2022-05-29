c7e5e2fedfabcbbf6a9d33e6bb7110739a6d6dc1
krzysztof <krzysztof.pacuszka@gmail.com>
2022-05-29 14:41:28 +0200

    Product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
               <Routes>
                   {" "}
                   <Route path='/' element={<HomeScreen />} exact />
-                  <Route path='/product/:id' element={<ProductScreen />} exact />
+                  <Route path='/product/:id' element={<ProductScreen />} />
               </Routes>
           </Container>
       </main>
