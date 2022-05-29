0ec06059a19ce61bd3e3efb792a1947439566ca9
krzysztof <krzysztof.pacuszka@gmail.com>
2022-05-29 14:09:40 +0200

    React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Header from "./components/header";
 import Footer from "./components/footer";
 import HomeScreen from "./screens/home_screen";
+import ProductScreen from "./screens/product_screen";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
         <Header />
       <main className="py-3">
           <Container >
-              <HomeScreen />
+              <Routes>
+                  {" "}
+                  <Route path='/' element={<HomeScreen />} exact />
+                  <Route path='/product/:id' element={<ProductScreen />} exact />
+              </Routes>
           </Container>
       </main>
         <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/rating.js
+++ frontend/src/components/rating.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React ,{useEffect, useState} from "react";
 
 const Rating = ({prodRating, numReviews, color = "#f8e825", id}) => {
     const [startRating, setStartRating] = useState([])
